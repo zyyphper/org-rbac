f197eb598abe6a26a35f4e5783c8a9a87d4286c6
zhengyuying <140879077+zyyphper@users.noreply.github.com>
2023-09-14 15:02:40 +0800

    fix bug

--- database/migrations/2023_09_10_061216_create_org_tables.php
+++ database/migrations/2023_09_10_061216_create_org_tables.php
@@ -22,97 +22,97 @@ class CreateOrgTables extends Migration
     public function up()
     {
         Schema::create(config('org.database.users_table'), function (Blueprint $table) {
-            $table->bigInteger('id')->nullable(false);
-            $table->bigInteger('platform_id')->nullable(false);
-            $table->bigInteger('company_id')->nullable(false);
+            $table->bigInteger('id');
+            $table->bigInteger('platform_id');
+            $table->bigInteger('company_id');
             $table->string('username', 64)->unique();
             $table->string('password', 60);
             $table->string('name',32);
             $table->string('avatar')->nullable();
-            $table->tinyInteger('is_admin')->nullable(false)->default(0);
-            $table->tinyInteger('status')->nullable(false)->default(1);
+            $table->tinyInteger('is_admin')->default(0);
+            $table->tinyInteger('status')->default(1);
             $table->integer('sort')->default(0);
             $table->string('remember_token', 100)->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
         Schema::create(config('org.database.user_infos_table'), function (Blueprint $table) {
-            $table->bigInteger('user_id')->nullable(false);
-            $table->char('phone', 12);
-            $table->string('email', 32);
-            $table->tinyInteger('is_check_identity')->nullable(false)->default(0);
-            $table->string('realname',16);
-            $table->char('identity_code',18);
+            $table->bigInteger('user_id');
+            $table->char('phone', 12)->nullable();
+            $table->string('email', 32)->nullable();
+            $table->tinyInteger('is_check_identity')->default(0);
+            $table->string('realname',16)->nullable();
+            $table->char('identity_code',18)->nullable();
             $table->timestamps();
         });
 
         Schema::create(config('org.database.menu_table'), function (Blueprint $table) {
-            $table->increments('id')->nullable(false);
+            $table->increments('id');
             $table->integer('parent_id')->default(0);
             $table->integer('order')->default(0);
             $table->string('title', 50);
-            $table->string('icon', 50);
+            $table->string('icon', 50)->nullable();
             $table->string('uri')->nullable();
-            $table->tinyInteger('type')->nullable(false)->default(1);
-            $table->tinyInteger('is_admin')->nullable(false)->default(0);
+            $table->tinyInteger('type')->default(1);
+            $table->tinyInteger('is_admin')->default(0);
             $table->timestamps();
         });
         Schema::create(config('org.database.platform_menu_table'), function (Blueprint $table) {
-            $table->integer('menu_id')->nullable(false);
-            $table->bigInteger('platform_id')->nullable(false);
+            $table->integer('menu_id');
+            $table->bigInteger('platform_id');
             $table->timestamps();
         });
         Schema::create(config('org.database.platforms_table'), function (Blueprint $table) {
-            $table->bigInteger('id')->nullable(false);
+            $table->bigInteger('id');
             $table->string('name',64);
-            $table->tinyInteger('is_admin')->nullable(false)->default(0);
-            $table->tinyInteger('status')->nullable(false)->default(1);
+            $table->tinyInteger('is_admin')->default(0);
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
 
         Schema::create(config('org.database.companies_table'), function (Blueprint $table) {
-            $table->bigInteger('id')->nullable(false);
-            $table->bigInteger('parent_id')->nullable(false)->default(0);
+            $table->bigInteger('id');
+            $table->bigInteger('parent_id')->default(0);
             $table->bigInteger('platform_id');
             $table->string('name', 32);
-            $table->string('email', 32);
-            $table->string('phone', 12);
+            $table->string('email', 32)->nullable();
+            $table->string('phone', 12)->nullable();
             $table->integer('order')->default(0);
             $table->timestamps();
         });
         Schema::create(config('org.database.departments_table'), function (Blueprint $table) {
-            $table->bigInteger('id')->nullable(false);
-            $table->bigInteger('parent_id')->nullable(false)->default(0);
-            $table->bigInteger('company_id')->nullable(false);
-            $table->string('name', 32)->nullable(false);
+            $table->bigInteger('id');
+            $table->bigInteger('parent_id')->default(0);
+            $table->bigInteger('company_id');
+            $table->string('name', 32);
             $table->string('leader', 32)->nullable();
             $table->integer('order')->default(0);
             $table->timestamps();
         });
         Schema::create(config('org.database.duties_table'), function (Blueprint $table) {
             $table->integer('id');
-            $table->bigInteger('user_id')->nullable(false);
-            $table->bigInteger('department_id')->nullable(false);
-            $table->tinyInteger('department_type')->nullable(false)->default(1);
+            $table->bigInteger('user_id');
+            $table->bigInteger('department_id');
+            $table->tinyInteger('department_type')->default(1);
             $table->integer('sort')->default(0);
             $table->timestamps();
         });
         Schema::create(config('org.database.roles_table'), function (Blueprint $table) {
             $table->integer('id');
-            $table->bigInteger('platform_id')->nullable(false);
+            $table->bigInteger('platform_id');
             $table->string('name',50);
             $table->string('slug',50);
-            $table->tinyInteger('is_admin')->nullable(false)->default(0);
+            $table->tinyInteger('is_admin')->default(0);
             $table->timestamps();
         });
         Schema::create(config('org.database.role_duty_table'), function (Blueprint $table) {
-            $table->integer('role_id')->nullable(false);
-            $table->integer('duty_id')->nullable(false);
+            $table->integer('role_id');
+            $table->integer('duty_id');
             $table->timestamps();
         });
         Schema::create(config('org.database.role_menu_table'), function (Blueprint $table) {
-            $table->integer('role_id')->nullable(false);
-            $table->integer('menu_id')->nullable(false);
+            $table->integer('role_id');
+            $table->integer('menu_id');
             $table->timestamps();
         });
     }
